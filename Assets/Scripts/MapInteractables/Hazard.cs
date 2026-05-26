41d4989070eff1c8a651b9fff61e006bcaf73118
jeffreyluu <jeffreyhluu@gmail.com>
2026-05-26 01:51:06 -0700

    feat: simplified Hazard.cs to instant death, added JeffreyTestScene with player setup

--- Assets/Scripts/MapInteractables/Hazard.cs
+++ Assets/Scripts/MapInteractables/Hazard.cs
@@ -5,61 +5,14 @@ public class Hazard : MonoBehaviour
     public enum HazardType { DeathPit, Spike, Lava }
     public HazardType hazardType;
 
-    public float lavaDamageRate = 10f;
-    public float maxHealth = 100f;
-
-    private float currentHealth;
-    private bool isInLava = false;
-
-    void Start()
-    {
-        currentHealth = maxHealth;
-    }
-
-    void Update()
-    {
-        if (hazardType == HazardType.Lava && isInLava)
-        {
-            currentHealth -= lavaDamageRate * Time.deltaTime;
-            if (currentHealth <= 0)
-                TriggerDeath();
-        }
-    }
-
     void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
-
-        switch (hazardType)
-        {
-            case HazardType.DeathPit:
-                TriggerDeath();
-                break;
-            case HazardType.Spike:
-                TriggerDeath();
-                break;
-            case HazardType.Lava:
-                isInLava = true;
-                break;
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (!other.CompareTag("Player")) return;
-
-        if (hazardType == HazardType.Lava)
-        {
-            isInLava = false;
-            currentHealth = maxHealth;
-        }
+        TriggerDeath();
     }
 
     void TriggerDeath()
     {
-        isInLava = false;
-        currentHealth = maxHealth;
-        
         Respawn respawn = FindAnyObjectByType<Respawn>();
         if (respawn != null)
             respawn.PlayerDied();
