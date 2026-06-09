82e5aa36d871898f40d762b944765d5ff1ad175c
Tai <ttt1@uw.edu>
2026-06-09 00:16:28 -0700

    linked list

--- Assets/Scripts/Transitions/LevelTransition.cs
+++ Assets/Scripts/Transitions/LevelTransition.cs
@@ -23,6 +23,7 @@ public class LevelTransition : MonoBehaviour
             {
                 float finalTime = levelTimer.StopTimer();
                 string player = PlayerPrefs.GetString(UsernameManager.SavedNamePrefKey, "Anonymous");
+                print("reached");
                 LeaderboardManager.Instance.SubmitScore(LevelNumber, player, finalTime);
             }
 
